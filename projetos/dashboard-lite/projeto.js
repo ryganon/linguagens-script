8560d89fb54fb2444f1d8222eda1c0b439f461d0
Danilo Lucena <rygaweb@gmail.com>
2019-02-07 17:10:30 -0300

    Adicionando funções javascript.

--- projetos/dashboard-lite/projeto.js
+++ projetos/dashboard-lite/projeto.js
@@ -52,7 +52,7 @@ function exibePosicao(position) {
   * @param {*} arquivo 
   */
 function carregaCSVLocal(arquivo){
-    
+  
     $.ajax({
         type: "GET",
         //url: "data.txt",
@@ -123,10 +123,42 @@ function carregaJSONLocal(){
 } 
 
 /**
- * Criando os elementos de uma lista com o Material Design
- * @param {*} dados_heroi 
+ * Função que carrega um XML de URL
  */
-function criaElementoLista(dados_heroi){
+function carregaXMLRemoto(){
+  
+  urlxml = 'https://gist.githubusercontent.com/Abhishek249/6356baab2a5abb5008b6f836b78522cf/raw/4e49a809a4cf6cfb047616edff8ae39e9394ea3a/movies.xml';
+  
+  $.get(urlxml, function(dados){
+    parserXML(dados);});
+}
+
+/**
+ * Função que processa os dados de um XML
+ * @param {*} xml 
+ */
+function parserXML(xml){
+
+  dadosLista = "";
+  
+  xmlDoc = $.parseXML(xml);
+  $xml = $(xmlDoc);
+  
+  $filmes = $xml.find("Movie");
+  
+  $filmes.each(function(){
+    titulo = $(this).find('Title').text();
+    genero = $(this).find('Genre').text();
+    dadosLista += criaElementoLista([titulo, genero]);
+  });
+  document.getElementById("projeto_lista").innerHTML = dadosLista;
+}
+
+/**
+ * Criando os elementos de uma lista com o Material Design
+ * @param {*} dados
+ */
+function criaElementoLista(dados){
   
   /*
   criando elemento lista
@@ -148,9 +180,9 @@ function criaElementoLista(dados_heroi){
  var item_lista = '<li class="mdl-list__item mdl-list__item--three-line">';
      item_lista += '<span class="mdl-list__item-primary-content">';
      item_lista += '<i class="material-icons mdl-list__item-avatar">person</i>';
-     item_lista += '<span>'+ dados_heroi[0] +'</span>';
+     item_lista += '<span>'+ dados[0] +'</span>';
      item_lista += '<span class="mdl-list__item-text-body">';
-     item_lista += dados_heroi[1];
+     item_lista += dados[1];
      item_lista += '</span></span></li>'; 
   
  return item_lista;
